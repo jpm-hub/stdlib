cd2bb50b5fcbad88df5fb12a95544155c08d48fa
sunny <sunny@sunnys-MBP.HME.COM>
2025-12-10 15:56:47 -0500

    fixed builder patern on class executor

--- jpm/ClassExecuter.java
+++ jpm/ClassExecuter.java
@@ -82,27 +82,7 @@ public class ClassExecuter {
 	}
 
 	public ClassExecuter build(String methodName, Object... typesAndArgs) {
-		if (typesAndArgs.length % 2 != 0) {
-			throw new IllegalArgumentException("typesAndArgs must be in pairs of (Class, Object)");
-		}
-		Class<?> localInstance;
-		if (instance == null) {
-			localInstance = clazz;
-		} else {
-			localInstance = instance.getClass();
-		}
-		try {
-			Class<?>[] types = new Class<?>[typesAndArgs.length / 2];
-			Object[] args = new Object[typesAndArgs.length / 2];
-			for (int i = 0; i < typesAndArgs.length; i += 2) {
-				types[i / 2] = (Class<?>) typesAndArgs[i];
-				args[i / 2] = typesAndArgs[i + 1];
-			}
-			localInstance.getDeclaredMethod(methodName, types).invoke(localInstance, args);
-			return this;
-		} catch (Exception e) {
-			System.err.println("Error calling method " + methodName + " on class " + className + ": " + e);
-			throw new RuntimeException(e);
-		}
+		call(methodName, typesAndArgs);
+		return this;
 	}
 }
